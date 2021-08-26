f0319e971745e9e11ff567131793ac68ca8cec6a
randyrafaelcg <randyrafaelcg@gmail.com>
2021-08-26 10:21:20 -0400

    se agrego formulario nombre y edad y boton saludat

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
-alert('Presiona OK si estas liste para empezar con javascript');
+//alert('Presiona OK si estas liste para empezar con javascript');
+const nombre = document.querySelector("#nombre-input");
+const form = document.querySelector("#saludador-form");
+const edad = document.querySelector("#edad-input");
+
+form.addEventListener("submit", (event) => {
+  alert("Hola " + nombre.value + " "+ edad.value);
+});
+
+
+
