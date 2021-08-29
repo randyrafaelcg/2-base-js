4232c71aef3d3efda43f94811f352894da60e1b4
randyrafaelcg <randyrafaelcg@gmail.com>
2021-08-29 14:35:53 -0400

    se agrego el saludo personalizado segun la hora del dia

--- src/App.js
+++ src/App.js
@@ -4,16 +4,44 @@ const form = document.querySelector("#saludador-form");
 const edad = document.querySelector("#edad-input");
 const genero=document.querySelector("#genero-input");
 
+var today = new Date();
+var hora=today.getHours();
+
 
   form.addEventListener("submit", (event) => {
-    if(edad.value>30 && genero.value=="Masculino")
-      alert("Hola Señor " + nombre.value);
-    else if(edad.value>30 && genero.value=="Femenino")
-      alert("Hola señora " + nombre.value);
-      else if(genero.value=="Otro" && edad.value>30)
-      alert("Sea Ud bienvenide " + nombre.value);
-      else
-      alert("Hola " + nombre.value);
+    if(hora>=12 && hora <=18)
+    {
+      if(edad.value>30 && genero.value=="Masculino")
+        alert("Buenas tardes Sr. " + nombre.value);
+      else if(edad.value>30 && genero.value=="Femenino")
+        alert("Buenas tardes Sra. " + nombre.value);
+        else if(genero.value=="Otro" && edad.value>30)
+        alert("Buenas tardes Sx." + nombre.value);
+        else
+        alert("Buenas tardes " + nombre.value);
+    }
+    else if(hora>18 && hora <=23)
+    {
+      if(edad.value>30 && genero.value=="Masculino")
+        alert("Buenas noches Sr. " + nombre.value);
+      else if(edad.value>30 && genero.value=="Femenino")
+        alert("Buenas noches Sra. " + nombre.value);
+        else if(genero.value=="Otro" && edad.value>30)
+        alert("Buenas noches Sx. " + nombre.value);
+        else
+        alert("Buenas noches " + nombre.value);
+    }
+    else
+    {
+      if(edad.value>30 && genero.value=="Masculino")
+        alert("Buenos dias Sr. " + nombre.value);
+      else if(edad.value>30 && genero.value=="Femenino")
+        alert("Buenos dias Sra. " + nombre.value);
+        else if(genero.value=="Otro" && edad.value>30)
+        alert("Buenos dias Sx." + nombre.value);
+        else
+        alert("Buenos dias " + nombre.value);
+    }
   });
 
 
