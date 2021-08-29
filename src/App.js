33ddbc6dde7c66cd5ad3c2c93c2c4ccfd376dd8d
randyrafaelcg <randyrafaelcg@gmail.com>
2021-08-29 11:16:58 -0400

    Se aumento la funcion para saludar segun el genero

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@
 const nombre = document.querySelector("#nombre-input");
 const form = document.querySelector("#saludador-form");
 const edad = document.querySelector("#edad-input");
+const genero=document.querySelector("#genero-input");
 
-form.addEventListener("submit", (event) => {
-  alert("Hola " + nombre.value + " "+ edad.value);
-});
+
+  form.addEventListener("submit", (event) => {
+    if(edad.value>30 && genero.value=="Masculino")
+      alert("Hola Señor " + nombre.value);
+    else if(edad.value>30 && genero.value=="Femenino")
+      alert("Hola señora " + nombre.value);
+      else if(genero.value=="Otro" && edad.value>30)
+      alert("Sea Ud bienvenide " + nombre.value);
+      else
+      alert("Hola " + nombre.value);
+  });
+
+
+
 
 
 
