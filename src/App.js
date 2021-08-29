4d0f41a5abf5062e64e8f5d2c55285a302f75339
randyrafaelcg <randyrafaelcg@gmail.com>
2021-08-29 15:06:23 -0400

    Se agrego la opcion para elegir idioma ingles

--- src/App.js
+++ src/App.js
@@ -3,12 +3,48 @@ const nombre = document.querySelector("#nombre-input");
 const form = document.querySelector("#saludador-form");
 const edad = document.querySelector("#edad-input");
 const genero=document.querySelector("#genero-input");
+const idioma=document.querySelector("#idioma-input");
 
 var today = new Date();
 var hora=today.getHours();
 
 
   form.addEventListener("submit", (event) => {
+    if(idioma.value=="Ingles")
+      if(hora>=12 && hora <=18)
+      {
+        if(edad.value>30 && genero.value=="Masculino")
+          alert("Good afternoon Mr. " + nombre.value);
+        else if(edad.value>30 && genero.value=="Femenino")
+          alert("Good afternoon Mrs. " + nombre.value);
+          else if(genero.value=="Otro" && edad.value>30)
+          alert("Good afternoon Mx." + nombre.value);
+          else
+          alert("Good afternoon " + nombre.value);
+      }
+      else if(hora>18 && hora <=23)
+      {
+        if(edad.value>30 && genero.value=="Masculino")
+          alert("Good evening Mr. " + nombre.value);
+        else if(edad.value>30 && genero.value=="Femenino")
+          alert("Good evening Mrs. " + nombre.value);
+          else if(genero.value=="Otro" && edad.value>30)
+          alert("Good evening Mx.. " + nombre.value);
+          else
+          alert("Good evening " + nombre.value);
+      }
+      else
+      {
+        if(edad.value>30 && genero.value=="Masculino")
+          alert("Good morning Mr. " + nombre.value);
+        else if(edad.value>30 && genero.value=="Femenino")
+          alert("Good morning Mrs. " + nombre.value);
+          else if(genero.value=="Otro" && edad.value>30)
+          alert("Good morning Mx." + nombre.value);
+          else
+          alert("Good morning " + nombre.value);
+      }
+    else
     if(hora>=12 && hora <=18)
     {
       if(edad.value>30 && genero.value=="Masculino")
